f2afb200f8d0c7e1cd99cb42e32f6715fa069930
Speedy <origino@bk.ru>
2018-11-05 22:52:46 +0300

    Update lfour.py

--- lfour.py
+++ lfour.py
@@ -1,10 +1,12 @@
+print('This program calculates factorial.')
 def factorial(n):
 	if n == 0:
 		return 1
 	return n * factorial(n - 1)
 
 
-print(factorial(3))
 
+user_input = input('Enter a number:')
+print('Result:', factorial(user_input))
 
 
